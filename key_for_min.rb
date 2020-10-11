37cdd6bf730b9f7a5aaef2c1d308d9866ab22364
Marc Steele <marcsteele2014@gmail.com>
2020-10-11 19:23:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+lowest = 1000000000
   name_hash.each do |key, value|
-    if value < lowest or lowest == undefined
+    if value < lowest
       lowest = value
     end
   end
