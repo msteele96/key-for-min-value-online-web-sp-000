5f0b0c0eadffa04c5335d39963ba7d6dc20b77dc
Marc Steele <marcsteele2014@gmail.com>
2020-10-11 19:20:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,9 @@
 def key_for_min_value(name_hash)
   lowest = ""
   name_hash.each do |key, value|
-    if value < lowest or lowest == ""
+    if value < lowest or lowest.is_a? == string
+
+    end
       lowest = value
     end
   end
